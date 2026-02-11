1df53291fe5b4bcbde6cfc0498cd130d72560689
Va6gn <2810727328@qq.com>
2026-02-11 14:46:12 +0800

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -1,11 +1,11 @@
-﻿GLOBAL.setmetatable(env, { __index = function(t, k) return GLOBAL.rawget(GLOBAL, k) end })
+GLOBAL.setmetatable(env, { __index = function(t, k) return GLOBAL.rawget(GLOBAL, k) end })
 
 local EVENT_COUNT = 1
-local NEWS_STYLE = "formal"
+local NEWS_STYLE = "playful"
 
 local function GetConfig()
     EVENT_COUNT = GetModConfigData("event_count") or 1
-    NEWS_STYLE = GetModConfigData("news_style") or "formal"
+    NEWS_STYLE = GetModConfigData("news_style") or "playful"
 end
 
 local NEWS_EVENTS = {
